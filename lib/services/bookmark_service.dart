f415917c587f64443d2d9dc6e76d462d8204fd88
bereketheramo25@gmail.com <bereketheramo25@gmail.com>
2024-02-15 16:50:12 +0300

    initial

---
+++ lib/services/bookmark_service.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter_news_app/models/new_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class BookmarkService {
+  Future<List<NewModel>> getbookmarks() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? bookmarksStringList = prefs.getStringList("bookmarks");
+    if (bookmarksStringList != null) {
+      return bookmarksStringList
+          .map((e) => NewModel.fromJson(jsonDecode(e)))
+          .toList();
+    } else {
+      return [];
+    }
+  }
+
+  Future<bool> inBookmarks(NewModel newModel) async {
+    List<NewModel> bookmarks = await getbookmarks();
+    return bookmarks
+        .where((element) => element.url == newModel.url)
+        .toList()
+        .isNotEmpty;
+  }
+
+  Future<void> toggleBookmark(NewModel newModel) async {
+    List<NewModel> bookmarks = await getbookmarks();
+    if (await inBookmarks(newModel)) {
+      //write your code here
+    } else {
+      //write your code here
+    }
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList(
+        "bookmarks", bookmarks.map((e) => jsonEncode(e.toJson())).toList());
+  }
+}
